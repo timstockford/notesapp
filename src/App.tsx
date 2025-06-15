bc5ade37effdba66921b6d7adc5771d70ad143a3
Tim Stockford <tdstockford@outlook.com>
2025-06-15 11:09:21 -0400

    added count button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={reactLogo} className="logo react" alt="React logo" />
         <h1>Hello from Amplify</h1>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
       </header>
     </div>
   );
